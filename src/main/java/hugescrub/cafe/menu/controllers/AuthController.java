1cc665e3277bcc9896afa1521fa304cb81e39414
hugescrub <hexi9001@gmail.com>
2022-07-23 19:18:08 +0300

    Authentication controller and important services

---
+++ src/main/java/hugescrub/cafe/menu/controllers/AuthController.java
@@ -0,0 +1,92 @@
+package hugescrub.cafe.menu.controllers;
+
+import hugescrub.cafe.menu.models.User;
+import hugescrub.cafe.menu.payload.request.UserRegisterRequest;
+import hugescrub.cafe.menu.payload.request.UserLoginRequest;
+import hugescrub.cafe.menu.payload.response.UserInfoResponse;
+import hugescrub.cafe.menu.payload.response.MessageResponse;
+import hugescrub.cafe.menu.repository.UserRepository;
+import hugescrub.cafe.menu.security.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/private/auth")
+public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthController(UserRepository userRepository, PasswordEncoder encoder, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody UserLoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+            List<String> roles = userDetails.getAuthorities().stream()
+                    .map(item -> item.getAuthority())
+                    .collect(Collectors.toList());
+
+            return ResponseEntity
+                    .ok(new UserInfoResponse(
+                            userDetails.getId(),
+                            userDetails.getUsername(),
+                            userDetails.getEmail(),
+                            roles));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserRegisterRequest registerRequest) {
+
+        // Validation
+        if (userRepository.existsByUsername(registerRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Username is already taken."));
+        }
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Email is already taken."));
+        }
+
+        // Create new user account
+        User user = new User(
+                registerRequest.getUsername(),
+                registerRequest.getEmail(),
+                encoder.encode(registerRequest.getPassword())
+        );
+
+        userRepository.save(user);
+        return ResponseEntity
+                .ok(new MessageResponse("User registered successfully"));
+    }
+}
---
+++ src/main/java/hugescrub/cafe/menu/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package hugescrub.cafe.menu.security.services;
+
+import hugescrub.cafe.menu.models.User;
+import hugescrub.cafe.menu.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return UserDetailsImpl.build(user);
+    }
+}
