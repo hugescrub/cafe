6e77f18c6a27cb67ef46ee4572c8785b25038adf
hugescrub <hexi9001@gmail.com>
2022-07-27 01:54:04 +0300

    Simple menu endpoints added

--- src/main/java/hugescrub/cafe/menu/controllers/MenuController.java
+++ src/main/java/hugescrub/cafe/menu/controllers/MenuController.java
@@ -1,13 +1,16 @@
 package hugescrub.cafe.menu.controllers;
 
 import hugescrub.cafe.menu.models.Menu;
+import hugescrub.cafe.menu.payload.response.MessageResponse;
 import hugescrub.cafe.menu.repository.MenuRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @Slf4j
@@ -24,16 +27,46 @@ public class MenuController {
     }
 
     @GetMapping("/all")
-    public List<Menu> getMenus(Authentication authentication) {
+    public List<Menu> getAllMenus(Authentication authentication) {
         log.info("Accessing endpoint with authorities: " + authentication.getAuthorities());
         return menuRepository.findAll();
     }
 
     @GetMapping("/{title}")
     public ResponseEntity<?> getByTitle(@PathVariable String title) {
-        Menu menu = menuRepository.findByTitle(title);
-        return ResponseEntity
-                .ok()
-                .body(menu);
+        if (menuRepository.existsByTitle(title)) {
+            Menu menu = menuRepository.findByTitle(title);
+            return ResponseEntity
+                    .ok()
+                    .body(menu);
+        } else {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Nothing was found with such title."));
+        }
+    }
+
+    /**
+     * Getting only those menus available starting from specific time (Ex. 08:00, 10:00 etc).
+     *
+     * @param availableFrom request field, String data type is applied since
+     *                      LocalTime is not applicable due to serialization problems.
+     *
+     * @return returns list for all corresponding entries.
+     */
+    @GetMapping("/available")
+    public ResponseEntity<?> getByAvailableFrom(@RequestBody String availableFrom) {
+        LocalTime time = LocalTime.parse(new JSONObject(availableFrom).getString("availableFrom"));
+        if (menuRepository.existsByAvailableFrom(time)) {
+            List<Menu> menuList = menuRepository.findByAvailableFrom(time);
+            return ResponseEntity
+                    .ok()
+                    .body(menuList);
+        } else {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Nothing was found with time provided."));
+        }
+
     }
 }
