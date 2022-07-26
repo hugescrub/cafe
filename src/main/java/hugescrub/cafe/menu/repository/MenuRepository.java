effe7b20a23e51b2c24b5e430a4c66c0c7c0d65b
hugescrub <hexi9001@gmail.com>
2022-07-27 01:58:11 +0300

    Menu repository additions

--- src/main/java/hugescrub/cafe/menu/repository/MenuRepository.java
+++ src/main/java/hugescrub/cafe/menu/repository/MenuRepository.java
@@ -4,14 +4,24 @@ import hugescrub.cafe.menu.models.EType;
 import hugescrub.cafe.menu.models.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalTime;
 import java.util.List;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Boolean existsByTitle(String title);
+
+    Boolean existsByAvailableFrom(LocalTime availableFrom);
+
+    Boolean existsByAvailableUntil(LocalTime availableUntil);
+
     Menu findByTitle(String title);
 
     List<Menu> findByAvailableFromAndAvailableUntil(LocalTime availableFrom, LocalTime availableUntil);
+
+    List<Menu> findByAvailableFrom(LocalTime availableFrom);
+
+    List<Menu> findByAvailableUntil(LocalTime availableUntil);
 
     List<Menu> findAllByType(EType type);
 }
