35f2e759c7c9574c4bd25c1f8d69ae890b461e9b
hugescrub <hexi9001@gmail.com>
2022-08-01 18:53:41 +0300

    Add item service method

--- src/main/java/hugescrub/cafe/menu/security/services/MenuService.java
+++ src/main/java/hugescrub/cafe/menu/security/services/MenuService.java
@@ -1,7 +1,9 @@
 package hugescrub.cafe.menu.security.services;
 
 import hugescrub.cafe.menu.dto.MenuDto;
+import hugescrub.cafe.menu.models.Item;
 import hugescrub.cafe.menu.models.Menu;
+import hugescrub.cafe.menu.repository.ItemRepository;
 import hugescrub.cafe.menu.repository.MenuRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +14,15 @@ import org.springframework.stereotype.Service;
 public class MenuService {
 
     private final MenuRepository menuRepository;
+    private final ItemRepository itemRepository;
 
     @Autowired
-    public MenuService(MenuRepository menuRepository) {
+    public MenuService(MenuRepository menuRepository, ItemRepository itemRepository) {
         this.menuRepository = menuRepository;
+        this.itemRepository = itemRepository;
     }
 
-    public void build (MenuDto menuDto){
+    public void build(MenuDto menuDto) {
         Menu menu = new Menu();
         menu.setType(menuDto.getType());
         menu.setTitle(menuDto.getTitle());
@@ -28,12 +32,27 @@ public class MenuService {
         log.info("New menu added.\n" + menu);
     }
 
-    public void update (MenuDto menuDto){
+    public void update(MenuDto menuDto) {
+        // get menu and item objects
         Menu menu = menuRepository.findByTitle(menuDto.getTitle());
+
+        // set new fields for existing menu
         menu.setType(menuDto.getType());
         menu.setAvailableFrom(menuDto.getAvailableFrom());
         menu.setAvailableUntil(menuDto.getAvailableUntil());
+
+        // save updated menu
         menuRepository.save(menu);
         log.info("Menu updated: " + menu.getTitle() + "\n" + menu);
+    }
+
+    public void addItem(String itemName, String menuTitle) {
+        // get menu and item objects
+        Menu menu = menuRepository.findByTitle(menuTitle);
+        Item item = itemRepository.findByName(itemName);
+
+        // add a new item for existing menu
+        menu.getItems().add(item);
+        menuRepository.save(menu);
     }
 }
