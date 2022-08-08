fbcff5513fbff7b51a97aa105f81f153d361d6f9
hugescrub <hexi9001@gmail.com>
2022-08-08 16:31:40 +0300

    Items-table on modal for selected menu

--- client/src/modal/Modal.js
+++ client/src/modal/Modal.js
@@ -1,40 +1,69 @@
-import React from 'react';
+import React from "react";
 import "./Modal.css";
 
 export default class Modal extends React.Component {
-
   state = {
-    isOpen: false
-  }
+    isOpen: false,
+  };
 
   render() {
     const {
-      menu: { id, type, title, img, availableFrom, availableUntil, items }
-    } = this.props
+      menu: { id, type, title, img, availableFrom, availableUntil, items },
+    } = this.props;
 
     console.log(this.props);
 
-    return(
+    return (
       <React.Fragment>
-      <button className="button" onClick={() => this.setState({isOpen: true})}>
-        About menu
-      </button>
+        <button
+          className="button"
+          onClick={() => this.setState({ isOpen: true })}
+        >
+          About menu
+        </button>
         {this.state.isOpen && (
-        <div className="modal">
-          <div className="modal-body">
-          <header>
-            <h1>{title}</h1>
-          </header>
-          <h4 className="price-availability">Between {availableFrom} - {availableUntil}</h4>
-          <p className="item-text">{type}</p>
-          <p> {items} </p>
-            <button className="button" onClick={() => this.setState({isOpen: false})}>
-            Back
-            </button>
+          <div className="modal">
+            <div className="modal-body">
+              <header>
+                <h1>{title}</h1>
+                <h4 className="price-availability">
+                  Between {availableFrom} - {availableUntil}
+                </h4>
+              </header>
+              <div>
+                <h1> Available dishes </h1>
+                <div className="modal-underline"></div>
+                <div>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Price</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {items.map((item) => (
+                        <tr>
+                          <td>{item.name}</td>
+                          <td>{item.itemType}</td>
+                          <td>{item.price}&nbsp;₽</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <button
+                className="button"
+                onClick={() => this.setState({ isOpen: false })}
+              >
+                Back
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </React.Fragment>
-    )
+    );
   }
 }
