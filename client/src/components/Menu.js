08aa8050efe5cf7655c372e47014ef4626de8a21
hugescrub <hexi9001@gmail.com>
2022-08-09 17:55:40 +0300

    Changed structure

---
+++ client/src/components/Menu.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+export default function Menu(props) {
+  const [Menu, setMenu] = useState([]);
+
+  const { id, type, title, img, availableFrom, availableUntil, items } =
+    props.menu;
+
+  const getData = () => {
+    fetch("http://localhost:8080/menu/" + title)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setMenu(res);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="section-center">
+      <article key={id} className="menu-item">
+        <img src={img} alt={title} className="photo" />
+        <div className="item-info">
+          <header>
+            <h4>{title}</h4>
+          </header>
+          <h4 className="price-availability">
+            Between {availableFrom} - {availableUntil}
+          </h4>
+          <p className="item-text">{type}</p>
+          <p> {items} </p>
+        </div>
+      </article>
+    </div>
+  );
+}
