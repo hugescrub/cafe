48567cda1e1230b5a5a93d88457e7c172569487b
hugescrub <hexi9001@gmail.com>
2022-08-10 21:05:54 +0300

    Routes

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import MenuList from "./MenuList";
 import Item from "./Item";
 
 const Main = () => {
+  const location = useLocation();
+  const searchType = useLocation()
+    .pathname.substring(location.pathname.lastIndexOf("/") + 1)
+    .toUpperCase();
 
   return (
     <Routes>
-      <Route path="/" element={<MenuList />} />
-      <Route path="/items/:id" element={<Item item/>} />
+      <Route path="/" element={<MenuList type="ALL" />} />
+      <Route path="/menus/:type" element={<MenuList type={searchType} />} />
+      <Route path="/items/:id" element={<Item item />} />
     </Routes>
   );
 };
