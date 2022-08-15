92e8be713a70e1c5680219f9585ac86b46f54b49
hugescrub <hexi9001@gmail.com>
2022-08-15 14:58:30 +0300

    Admin routing

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import MenuList from "./MenuList";
 import Item from "./Item";
 import Login from "./login/Login"
+import Admin from "./admin/Admin"
+import Actions from "./admin/Actions"
 
 const Main = () => {
   const location = useLocation();
@@ -15,8 +17,10 @@ const Main = () => {
     <Routes>
       <Route path="/" element={<MenuList type="ALL" />} />
       <Route path="/menus/:type" element={<MenuList type={searchType} />} />
-      <Route path="/items/:id" element={<Item />} />
-      <Route path="/authorize" element={<Login />}/>
+      <Route path="/items/:id" element={<Item item/>} />
+      <Route path="/authorize" element={<Login />} />
+      <Route path="/admin" element={localStorage.getItem('data') ? <Admin /> : <Navigate to="/authorize" />} />
+      <Route path="/actions" element={<Actions />} />
     </Routes>
   );
 };
