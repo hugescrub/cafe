5caee57169d75ad0227a3e148e35da0e6fe3c72f
hugescrub <hexi9001@gmail.com>
2022-08-15 14:57:04 +0300

    Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return(
+        <footer-section class="footer-distributed">
+        <div class="footer-left">
+          <p class="footer-links">
+            <a class="link-1" href="#">About</a>
+            <a href="#">Contact</a>
+          </p>
+          <p>Made by hugescrub </p>
+        </div>
+        </footer-section>
+    )
+}
