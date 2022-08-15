abfc73267e2e77f690c370d6e874d9cdd00b8e14
hugescrub <hexi9001@gmail.com>
2022-08-15 14:51:14 +0300

    Fixed typo

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,21 @@ export default function App() {
   return (
     <main>
       <Main />
-      <footer-section class="footer-distributed">
-        <div class="footer-right">
+      <header-section class="header-distributed">
+        <div class="header-right">
           <a href="https://github.com/hugescrub">Github</a>
         </div>
 
-        <div class="footer-left">
-          <p class="footer-links">
+        <div class="header-left">
+          <p class="header-links">
             <a class="link-1" href="/">
               Home
             </a>
             <a href="/authorize">Authorize</a>
             <a href="#">Pricing</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
           </p>
         </div>
-      </footer-section>
+      </header-section>
     </main>
   );
 }
