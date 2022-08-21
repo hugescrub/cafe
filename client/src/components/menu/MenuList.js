9605e0ada3d36f155a033efd63a78132263a6bac
hugescrub <hexi9001@gmail.com>
2022-08-21 23:33:58 +0300

    Menu cards

--- client/src/components/menu/MenuList.js
+++ client/src/components/menu/MenuList.js
@@ -54,23 +54,25 @@ export default function MenuList({ type }) {
           const { id, type, title, availableFrom, availableUntil, image } =
             menu;
           return (
-            <article key={id} className="menu-element">
-              <img
-                src={`data:image/jpeg;base64,${image}`}
-                alt={title}
-                className="photo"
-              />
-              <div className="item-info">
-                <header>
-                  <h4>{title}</h4>
-                </header>
-                <h4 className="price-availability">
-                  Between {availableFrom} - {availableUntil}
-                </h4>
-                <p className="item-text-main">{type}</p>
-                <Modal menu={menu} />
-              </div>
-            </article>
+            <div className="menu-card">
+              <article key={id} className="menu-element">
+                <img
+                  src={`data:image/jpeg;base64,${image}`}
+                  alt={title}
+                  className="photo"
+                />
+                <div className="item-info">
+                  <header>
+                    <h4>{title}</h4>
+                  </header>
+                  <h4 className="price-availability">
+                    Between {availableFrom} - {availableUntil}
+                  </h4>
+                  <p className="item-text-main">{type}</p>
+                  <Modal menu={menu} />
+                </div>
+              </article>
+            </div>
           );
         })}
       </div>
